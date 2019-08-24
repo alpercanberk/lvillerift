906da6bfaaa6e3c0498cde554903e09c41752a86
alpertucanberk <alper.tu.canberk@gmail.com>
2019-08-24 22:45:24 +0300

    trynna make the heroku work

---
+++ build/precache-manifest.e756d8b6da78ad80b4ed0523260c83b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978137b3f8d86dd2dcdaa7081002ea2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60a32870feac94a031f3",
+    "url": "/static/css/main.af69ce8d.chunk.css"
+  },
+  {
+    "revision": "db0e95b37d3dc87316c2",
+    "url": "/static/js/2.4f775e78.chunk.js"
+  },
+  {
+    "revision": "60a32870feac94a031f3",
+    "url": "/static/js/main.d6471a52.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
