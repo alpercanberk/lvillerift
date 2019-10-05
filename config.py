f918911fec3698d7c6bed97f38de5b5d4bcf305d
alpertucanberk <alper.tu.canberk@gmail.com>
2019-10-05 10:52:51 -0400

    ree

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/life'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
